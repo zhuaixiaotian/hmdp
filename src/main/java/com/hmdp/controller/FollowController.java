2c853d874b3ecfceaa9c3e964b4b3f6dc6a6a3db
zhuaixiaotian <2519496811@qq.com>
2023-08-15 00:25:14 +0800

    feat:关注&&取关

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,10 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -17,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/follow")
 public class FollowController {
 
+    @Autowired
+    private IFollowService followService;
+
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long followedId, @PathVariable("isFollow") Boolean isFollow) {
+        return followService.follow(followedId,isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followedId) {
+        return followService.isFollow(followedId);
+    }
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * <p>
@@ -17,4 +24,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Autowired
+    private FollowMapper followMapper;
+
+    @Override
+    public Result follow(Long followedId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        if (isFollow) {
+            Follow follow = new Follow();
+            follow.setFollowUserId(followedId);
+            follow.setUserId(userId);
+            follow.setCreateTime(LocalDateTime.now());
+            save(follow);
+        } else {
+            remove(new LambdaQueryWrapper<Follow>().eq(Follow::getUserId, userId)
+                    .eq(Follow::getFollowUserId, followedId));
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followedId) {
+        Long userId = UserHolder.getUser().getId();
+        LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<Follow>().eq(Follow::getUserId, userId)
+                .eq(Follow::getFollowUserId, followedId);
+        int count = count(queryWrapper);
+        return Result.ok(count > 0);
+    }
 }
